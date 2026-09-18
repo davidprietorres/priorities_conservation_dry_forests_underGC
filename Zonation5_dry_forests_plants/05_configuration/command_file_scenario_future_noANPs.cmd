e8effd78eae8f3e79329f4a41f318df16b797045
davidprietorres <75331523+davidprietorres@users.noreply.github.com>
2026-09-17 23:05:07 -0600

    Add files via upload

---
+++ Zonation5_dry_forests_plants/05_configuration/command_file_scenario_future_noANPs.cmd
@@ -0,0 +1,6 @@
+@setlocal
+@PATH="C:\Program Files\Zonation5";%PATH%
+
+z5 -waXi --mode=ABF --gui settings_current_scenario_futuro_noANPs.z5 ..\06_outputs\priority_future_noPA
+@pause
+
---
+++ Zonation5_dry_forests_plants/05_configuration/command_file_scenario_futuro_ANPs.cmd
@@ -0,0 +1,6 @@
+@setlocal
+@PATH="C:\Program Files\Zonation5";%PATH%
+
+z5 -wahXi --mode=ABF --gui settings_current_scenario_futuro_ANPs.z5 ..\06_outputs\priority_future_PAs
+@pause
+
